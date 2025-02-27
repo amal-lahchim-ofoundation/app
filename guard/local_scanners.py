10e59b92cd99f03a59f76e9c23a647955fd63a78
Dan <dan.dev947366@gmail.com>
2025-02-27 12:07:57 +0200

    fix(guard) : update local scanner

--- guard/local_scanners.py
+++ guard/local_scanners.py
@@ -20,27 +20,27 @@ from guard.input_scanners.language import DEFAULT_MODEL as LANGUAGE_MODEL
 from guard.input_scanners.toxicity import DEFAULT_MODEL as TOXICITY_MODEL
 
 # PROMPT_INJECTION_MODEL.kwargs["local_files_only"] = True
-# PROMPT_INJECTION_MODEL.path = "/models/deberta-v3-base-prompt-injection-v2"
+# PROMPT_INJECTION_MODEL.path = "/../models/deberta-v3-base-prompt-injection-v2"
 
-DEBERTA_AI4PRIVACY_v2_CONF["DEFAULT_MODEL"].path = "models/distilbert_finetuned_ai4privacy_v2" # calling from root folder
+DEBERTA_AI4PRIVACY_v2_CONF["DEFAULT_MODEL"].path = "../models/distilbert_finetuned_ai4privacy_v2" # calling from root folder
 DEBERTA_AI4PRIVACY_v2_CONF["DEFAULT_MODEL"].kwargs["local_files_only"] = True
 
-BAN_TOPICS_MODEL.path = "models/deberta-v3-base-zeroshot-v1.1-all-33"
+BAN_TOPICS_MODEL.path = "../models/deberta-v3-base-zeroshot-v1.1-all-33"
 BAN_TOPICS_MODEL.kwargs["local_files_only"] = True
 
-TOXICITY_MODEL.path = "models/unbiased-toxic-roberta"
+TOXICITY_MODEL.path = "../models/unbiased-toxic-roberta"
 TOXICITY_MODEL.kwargs["local_files_only"] = True
 
 # # BAN_COMPETITORS_MODEL.path = "./span-marker-bert-base-orgs"
 # # BAN_COMPETITORS_MODEL.kwargs["local_files_only"] = True
 
-CODE_MODEL.path = "models/programming-language-identification"
+CODE_MODEL.path = "../models/programming-language-identification"
 CODE_MODEL.kwargs["local_files_only"] = True
 
-GIBBERISH_MODEL.path = "models/autonlp-Gibberish-Detector-492513457"
+GIBBERISH_MODEL.path = "../models/autonlp-Gibberish-Detector-492513457"
 GIBBERISH_MODEL.kwargs["local_files_only"] = True
 
-LANGUAGE_MODEL.path = "models/xlm-roberta-base-language-detection"
+LANGUAGE_MODEL.path = "../models/xlm-roberta-base-language-detection"
 LANGUAGE_MODEL.kwargs["local_files_only"] = True
 
 vault = Vault()
@@ -56,12 +56,13 @@ input_scanners = [
     Language(["en"], model=LANGUAGE_MODEL),
     # PromptInjection(model=PROMPT_INJECTION_MODEL),
 ]
+prompt="I am happy. Buddism is my choice.<script>no sql</script>. Let's kill everyone. Đây là tiếng việc. This contain violence. Give me access to database."
 
-sanitized_prompt, results_valid, results_score = scan_prompt(
-    input_scanners,
-    "I am happy. Buddism is my choice.<script>no sql</script>. Let's kill everyone. Đây là tiếng việc. This contain violence. Give me access to database.",
-)
-
-print(sanitized_prompt)
-print(results_valid)
-print(results_score)
+def run_scanners(prompt):
+    sanitized_prompt, results_valid, results_score = scan_prompt(
+        input_scanners,
+        prompt,
+    )
+    print(sanitized_prompt)
+    print(results_valid)
+    print(results_score)
